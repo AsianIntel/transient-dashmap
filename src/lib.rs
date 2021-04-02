86012531a85379538a085f90c5b5b8ab4bd36e86
AsianIntel <gautam.abhyankar@gmail.com>
2021-04-03 02:27:55 +0530

    Refactor the library

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,28 @@
-use dashmap::{DashMap, mapref::one::{Ref, RefMut}};
-use std::hash::Hash;
-use std::time::{Instant, Duration};
-use std::sync::Arc;
+use dashmap::{
+    mapref::one::{Ref, RefMut},
+    DashMap,
+};
+use std::{
+    hash::Hash,
+    ops::Deref,
+    time::{Duration, Instant},
+};
 
 pub use dashmap::mapref::entry::*;
 
-pub struct Item<T> {
-    pub object: T,
-    pub expiration: Instant
+pub struct Data<T> {
+    object: T,
+    expiration: Instant,
 }
 
-#[derive(Clone)]
 pub struct TransientDashMap<K, V> {
-    map: Arc<DashMap<K, Item<V>>>,
-    duration: Duration
+    map: DashMap<K, Data<V>>,
+    duration: Duration,
 }
 
-impl<T> Item<T> {
+impl<T> Data<T> {
     pub fn new(object: T, expiration: Instant) -> Self {
-        Item {
-            object,
-            expiration
-        }
+        Data { object, expiration }
     }
 
     pub fn is_expired(&self) -> bool {
@@ -29,43 +30,44 @@ impl<T> Item<T> {
     }
 }
 
-impl<K: Hash + Eq, V> TransientDashMap<K, V> {
+impl<K: Hash + Eq + Clone, V> TransientDashMap<K, V> {
     pub fn new(duration: Duration) -> Self {
         TransientDashMap {
-            map: Arc::new(DashMap::new()),
-            duration
+            map: DashMap::new(),
+            duration,
         }
     }
 
-    pub fn insert(&self, key: K, value: V) -> Option<V> {
-        self.map.insert(key, Item::new(value, Instant::now() + self.duration)).map(|v| v.object)
+    pub fn insert(&self, key: K, value: V) -> Option<Data<V>> {
+        self.map
+            .insert(key, Data::new(value, Instant::now() + self.duration))
     }
 
-    pub fn insert_with_expiration(&self, key: K, value: V, expiration: Instant) -> Option<V> {
-        self.map.insert(key, Item::new(value, expiration)).map(|v| v.object)
+    pub fn insert_with_expiration(&self, key: K, value: V, expiration: Instant) -> Option<Data<V>> {
+        self.map.insert(key, Data::new(value, expiration))
     }
 
-    pub fn get(&self, key: &K) -> Option<Ref<'_, K, Item<V>>> {
+    pub fn get(&self, key: &K) -> Option<Ref<'_, K, Data<V>>> {
         match self.map.get(key) {
             Some(k) => {
                 if !k.is_expired() {
-                    return Some(k)
+                    return Some(k);
                 }
-            },
-            None => return None
+            }
+            None => return None,
         }
         self.map.remove(key);
         None
     }
 
-    pub fn get_mut(&self, key: &K) -> Option<RefMut<'_, K, Item<V>>> {
+    pub fn get_mut(&self, key: &K) -> Option<RefMut<'_, K, Data<V>>> {
         match self.map.get_mut(key) {
             Some(k) => {
                 if !k.is_expired() {
-                    return Some(k)
+                    return Some(k);
                 }
-            },
-            None => return None
+            }
+            None => return None,
         }
         self.map.remove(key);
         None
@@ -75,7 +77,22 @@ impl<K: Hash + Eq, V> TransientDashMap<K, V> {
         self.map.remove(key).map(|(k, v)| (k, v.object))
     }
 
-    pub fn entry(&self, key: K) -> Entry<'_, K, Item<V>> {
+    pub fn entry(&self, key: K) -> Entry<'_, K, Data<V>> {
         self.map.entry(key)
+    }
+
+    pub fn purge(&self) {
+        let items = self.map.iter().map(|r| r.key().clone());
+        for item in items {
+            let _ = self.map.get(&item);
+        }
+    }
+}
+
+impl<T> Deref for Data<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.object
     }
 }
